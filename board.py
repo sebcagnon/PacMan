459f9748e460cc4b79b910b017194e6a34b6e894
Sebastien Cagnon <scagnon@aldebaran-robotics.com>
2013-02-21 16:04:52 +0900

    tiles and board can now be printed

--- board.py
+++ board.py
@@ -6,28 +6,41 @@ Date: 2013-Feb-21
 class Tile:
     """An empty tile"""
     def __init__(self):
-        self.shape= " "
+        self.shape= "  "
 
-    def display(self):
+    def __str__(self):
         return str(self.shape)
+    
 
 class Wall(Tile):
     """A tile you can't go through"""
     def __init__(self):
-        self.shape= chr(1)
+        self.shape= '++'
 
 class Object(Tile):
     def __init__(self):
-        self.shape= chr(64)
+        self.shape= chr(64)*2
 
 class Board:
-    def __init__(self):
-        pass
+    def __init__(self, width=5, height=5, fromFile=None):
+        if fromFile:
+            self.map= eval(open(fromFile, 'r').read())
+        else:
+            self.map= [[Wall()]*(width+2)]
+            for _ in range(height):
+                self.map.append([Wall()]+[Tile()]*width+[Wall()])
+            self.map.append([Wall()]*(width+2))
 
-    def display(self):
-        pass
+    def __str__(self):
+        return '\n'.join([''.join([str(tile) for tile in line])
+                          for line in self.map])
+
+    def __getitem__(self, n):
+        return self.map[n]
 
 
 if __name__=='__main__':
     tiles= [Tile(), Wall(), Object()]
-    assert [tile.display() for tile in tiles]==[' ', '\x01', '@']
+    assert [str(tile) for tile in tiles]==['  ', '++', '@@']
+    board= Board(width=2,height=2)
+    assert str(board)=='++++++++\n++    ++\n++    ++\n++++++++'
