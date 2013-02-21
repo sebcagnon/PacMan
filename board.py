de49966f012418f428c90e6b08d53449cc7c4b79
Sebastien Cagnon <scagnon@aldebaran-robotics.com>
2013-02-21 16:31:23 +0900

    can now save boards

--- board.py
+++ board.py
@@ -35,8 +35,32 @@ class Board:
         return '\n'.join([''.join([str(tile) for tile in line])
                           for line in self.map])
 
-    def __getitem__(self, n):
-        return self.map[n]
+    def __getitem__(self, i):
+        return self.map[i]
+
+    def __setitem__(self, i, j=None):
+        return self.map[i]
+
+    def save(self, filePath):
+        count=0
+        try:
+            open(filePath, 'r')
+            print "The file already exist: " + filePath
+            while count<3:
+                ans=raw_input("Do you want to want to rewrite the file (Y/n)? ")
+                if ans=='Y':
+                    break
+                elif ans=='n':
+                    return False
+                else:
+                    print "Invalid input. Type 'Y' or 'n' without quotes"
+                count+=1
+        except IOError:
+            pass
+        if count==3:
+            return False
+        open(filePath, 'w').write(str(self))
+        return True
 
 
 if __name__=='__main__':
