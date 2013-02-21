537477557d4e529016968850915ee63b701adf82
Sebastien Cagnon <scagnon@aldebaran-robotics.com>
2013-02-21 18:12:49 +0900

    playing around with Tkinter for the window

--- game.py
+++ game.py
@@ -1,14 +1,28 @@
 import board
 import Tkinter
+import Image
 from Tkconstants import *
 
 b= board.Board(20,20)
+img= Image.new('1', (200,200))
+for i in range(10, 150, 20):
+	for j in range(10, 150, 20):
+		for i2 in range(10):
+			for j2 in range(10):
+				img.putpixel((i+i2, j+j2), 200)
+
+def myCallBack():
+    b[10][10]=board.Object()
+    label.configure(text=str(b))
+
 tk=Tkinter.Tk(className="PacMan")
 frame= Tkinter.Frame(tk, relief=RIDGE, borderwidth=2)
 frame.pack(fill=BOTH, expand=1)
-label= Tkinter.Label(frame, text=str(b))
+bmp= Tkinter.BitmapImage(name="bmp", data= img.tobitmap())
+label= Tkinter.Label(frame, text=str(b), font='TkFixedFont')
+label.bind(sequence='<Alt-A>', func=myCallBack)
 label.pack(fill=X, expand=1)
-button= Tkinter.Button(frame, text="Exit", command=tk.destroy)
+button= Tkinter.Button(frame, text="Exit", command=myCallBack)
 button.pack(side=BOTTOM)
 tk.mainloop()
 
