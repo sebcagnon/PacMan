60d4f8a869d5f784c40335df3ed31019c46c91a0
Sebastien Cagnon <cagnonsebastien@gmail.com>
2013-02-22 22:37:06 +0100

    I can now move an object around using the arrow keys

--- game.py
+++ game.py
@@ -4,6 +4,9 @@ import Image
 from Tkconstants import *
 
 b= board.Board(20,20)
+b[10][10]=board.Object()
+x=10
+y=10
 img= Image.new('1', (200,200))
 for i in range(10, 150, 20):
 	for j in range(10, 150, 20):
@@ -11,21 +14,37 @@ for i in range(10, 150, 20):
 			for j2 in range(10):
 				img.putpixel((i+i2, j+j2), 200)
 
-def myCallBack():
-    b[10][10]=board.Object()
+def move(dx,dy):
+    global x,y
+    b[x][y]= board.Tile()
+    x+=dx
+    y+=dy
+    b[x][y]= board.Object()
     label.configure(text=str(b))
+
+def onKey(event):
+    if event.keycode==111:
+        move(-1,0)
+    elif event.keycode==113:
+        move(0,-1)
+    elif event.keycode==114:
+        move(0,1)
+    elif event.keycode==116:
+        move(1,0)
+
 
 tk=Tkinter.Tk(className="PacMan")
 frame= Tkinter.Frame(tk, relief=RIDGE, borderwidth=2)
 frame.pack(fill=BOTH, expand=1)
 bmp= Tkinter.BitmapImage(name="bmp", data= img.tobitmap())
-##label= Tkinter.Label(frame, text=str(b), font='TkFixedFont')
-##label.bind(sequence='<Alt-A>', func=myCallBack)
-##label.pack(fill=X, expand=1)
-c = Tkinter.Canvas(tk, width=200, height=200); c.pack()
-c.create_image(0, 0, image = bmp, anchor=Tkinter.NW)
-button= Tkinter.Button(frame, text="Exit", command=myCallBack)
+label= Tkinter.Label(frame, text=str(b), font='TkFixedFont')
+label.bind(sequence='<Key>', func=onKey)
+label.pack(fill=X, expand=1)
+#c = Tkinter.Canvas(tk, width=200, height=200); c.pack()
+#c.create_image(0, 0, image = bmp, anchor=Tkinter.NW)
+button= Tkinter.Button(frame, text="Exit", command=tk.destroy)
 button.pack(side=BOTTOM)
+label.focus_set()
 tk.mainloop()
 
 print "exiting"
